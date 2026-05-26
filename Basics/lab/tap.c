4af5faa7ffd4cc88a3f938bf8dc2524b30583ec5
Jithendiran <jidesh0410@gmail.com>
2026-05-26 05:34:14 +0530

    tap discover completed

--- Basics/lab/tap.c
+++ Basics/lab/tap.c
@@ -17,6 +17,15 @@ Task : Open a virtual NIC device with name jitap
 #include <linux/if_ether.h>
 #include <arpa/inet.h>
 
+static const char * protoname(u_int16_t type){
+    switch (type)
+    {
+        case 0x0800:    return "IPv4";
+        case 0x0806:    return "ARP";
+        case 0x86DD:    return "IPv6";
+        default:        return "Unknown";
+    }
+}
 
 static const char * name = "jitap"; 
 int main(){
@@ -129,7 +138,7 @@ int main(){
     read the /usr/include/linux/if.h 's ifreq or man netdevice
     there is a ifru_flags
     */
-    interface.ifr_flags = IFF_TAP;
+    interface.ifr_flags = IFF_TAP | IFF_NO_PI; // 1st time remove `IFF_NO_PI` and run, then read the comment inside loop
     int res = ioctl(fd, TUNSETIFF, &interface);
     if(res < 0) {
         perror("interface config");
@@ -275,7 +284,7 @@ int main(){
     * If we use hardware interface we would directly use the socket step by name, so the read only happens through socket
     * also as per `man socket` read(2), recv(2) used for reading
     */
-    char buf[1024];
+    char buf[1500];
     
     while (1){
         /* 
@@ -284,7 +293,9 @@ int main(){
         2. return n <= requested -> available to read
         3. return -1 error, set tp errno 
         */
-        res = read(sfd, &buf, 1024);
+        // res = read(sfd, buf, 1500); // issue with socket descriptor, it is reading packet from all the available interface
+                                        // but need only jitap, so using fd for reading
+        res = read(fd, buf, 1500);
         if(res < 0){
             perror("Read socket");
             return 1;
@@ -313,6 +324,7 @@ int main(){
 
         
         struct ethhdr *frame = (struct ethhdr *)buf;
+        int proto = ntohs(frame->h_proto);
         printf("Src MAC: %02x:%02x:%02x:%02x:%02x:%02x\t",
             frame->h_source[0],frame->h_source[1],frame->h_source[2],
             frame->h_source[3],frame->h_source[4],frame->h_source[5]);
@@ -320,8 +332,45 @@ int main(){
             frame->h_dest[0],frame->h_dest[1],frame->h_dest[2],
             frame->h_dest[3],frame->h_dest[4],frame->h_dest[5]);
 
+        // to convert protol to String refer /usr/include/linux/if_ether.h
+        printf("Protocol: %s(0x%04x)\n", protoname(proto), proto);
+
+        // run without `IFF_NO_PI` flag
+        /*
+        Src MAC: 00:00:00:16:b2:6a      Dst MAC: 00:00:86:dd:33:33      Protocol: Unknown(0x1b37)
+        Src MAC: 00:00:00:16:32:00      Dst MAC: 00:00:86:dd:33:33      Protocol: Unknown(0x059d)
+        Src MAC: ff:37:f7:ea:32:00      Dst MAC: 00:00:86:dd:33:33      Protocol: Unknown(0x059d)
+        */
+
+        // in /usr/include/linux/if_ether.h there is no such protocols, and src/dst mac address is wrong
         
-        printf("Protocol: (0x%04x)\n",ntohs(frame->h_proto));
+        // print the hex dump
+        // printf("Size : %d\t",res);
+        // for(int i = 0; i < res; i++){
+        //     printf("0x%02x ",(unsigned char)buf[i]);
+        // }
+        // printf("\n");
+
+        /*
+            Size : 94       0x00 0x00 0x86 0xdd 0x33 0x33 0x00 0x00 0x00 0x16 0x0e 0x3d 0xe2 0x24 0x37 0xe8 0x86 0xdd 0x60 0x00 0x00 0x00 0x00 0x24 0x00 0x01 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0xff 0x02 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x16 0x3a 0x00 0x05 0x02 0x00 0x00 0x01 0x00 0x8f 0x00 0x37 0x7e 0x00 0x00 0x00 0x01 0x04 0x00 0x00 0x00 0xff 0x02 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x01 0xff 0x24 0x37 0xe8 
+        */
+       //-----------
+       /*
+       Terminal 2:
+       $ ip monitor link 
+        11: jitap: <BROADCAST,MULTICAST> mtu 1500 qdisc noop state DOWN group default 
+            link/ether 0e:3d:e2:24:37:e8 brd ff:ff:ff:ff:ff:ff
+        11: jitap: <BROADCAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UNKNOWN group default 
+            link/ether 0e:3d:e2:24:37:e8 brd ff:ff:ff:ff:ff:ff
+        11: jitap: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UNKNOWN group default 
+            link/ether 0e:3d:e2:24:37:e8 brd ff:ff:ff:ff:ff:ff
+       */
+
+       /* in output after 4 bytes (0x00 0x00 0x86 0xdd), we able to see dest mac address (0x33 0x33 0x00 0x00 0x00 0x16) then 
+       src mac address (0x0e 0x3d 0xe2 0x24 0x37 0xe8), then protocol  (0x86 0xdd)
+
+       so four bytes are prepended check /usr/include/linux/if_tun.h here, tun_pi comment says, if IFF_NO_PI is not set then 4 bytes will be prepended so add `IFF_NO_PI`
+       */
 
     }
     return 0;
